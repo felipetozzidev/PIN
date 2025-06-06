a7a3f3c545f87b0057ea9338a503560a1a8edcef
brenin.dev <brenosilveiradomingues@gmail.com>
2025-06-06 17:35:54 -0300

    Atualiza a configuração de conexão com o banco de dados e cria o arquivo index.php com a página "Em Densevolvimento"

--- config/conn.php
+++ config/conn.php
@@ -1,18 +1,18 @@
 <?php
 //Variaveis que armazenam os dados de conexão
-$localhost = "localhost";
-$user = "root";
-$passw = "";
-$banco = "IFApoia";
+$host = "77.37.127.2";
+$user = "u245002075_ifapoia_adm";
+$passw = "|wleg$~8I6";
+$banco = "u245002075_ifapoia";
 
 //conexão
-$conn = mysqli_connect($localhost, $user, $passw, $banco);
+$conn = mysqli_connect($host, $user, $passw, $banco);
 if (!$conn) {
     die("Não conectado");
 }
 
 //Faz o teste da conexão
-date_default_timezone_set('Brazil/East');
-mysqli_query($conn, "SET NAMES 'utf8'");
+// date_default_timezone_set('Brazil/East');
+// mysqli_query($conn, "SET NAMES 'utf8'");
 
 session_start();
